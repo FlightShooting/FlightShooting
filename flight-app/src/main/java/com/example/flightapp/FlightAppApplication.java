da52ae804e2b794c989216b20b8f4047c23eaacb
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-12-06 14:15:41 +0100

    que si que si

---
+++ flight-app/src/main/java/com/example/flightapp/FlightAppApplication.java
@@ -0,0 +1,18 @@
+package com.example.flightapp;
+
+import com.example.flightapp.aviones.Primario;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class FlightAppApplication {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(FlightAppApplication.class, args);
+        Primario p = new Primario( "1", 100, 1000);
+        p.run();
+    }
+
+}
