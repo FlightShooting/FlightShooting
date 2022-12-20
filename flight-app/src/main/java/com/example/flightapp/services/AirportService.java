7394a694f14824be821f444b8e0867167dd2faaa
chiarab22 <cbocc@myuax.com>
2022-12-20 10:25:01 +0100

    pom y firebase connection

--- flight-app/src/main/java/com/example/flightapp/services/AirportService.java
+++ flight-app/src/main/java/com/example/flightapp/services/AirportService.java
@@ -3,10 +3,36 @@ package com.example.flightapp.services;
 
 import org.springframework.stereotype.Controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 @Controller
 public class AirportService {
+    public void conectar() {
+        //conectar con la API
+      //  FirebaseOptions options = new FirebaseOptions.Builder() .setApiKey("AIzaSyC8Cv6m-EMb1WxemWUcuLRxz5lOBpdQHiY") .build();
+        //FirebaseApp.initializeApp(options);
 
-    @GetMapping("/airport")
+        try {
+            FileInputStream serviceAccount = new FileInputStream(file);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredential(FirebaseCredentials.fromCertificate("AIzaSyC8Cv6m-EMb1WxemWUcuLRxz5lOBpdQHiY"))
+                    .setDatabaseUrl(url)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: invalid service account credentials. See README.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // Shared Database reference
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+  //  @GetMapping("/airport")
 
 }
--- flight-app/src/main/java/com/example/flightapp/util/Constantes.java
+++
@@ -1,6 +0,0 @@
-package com.example.flightapp.util;
-
-public class Constantes {
-    public final static String URL = "https://airports-c14a9-default-rtdb.europe-west1.firebasedatabase.app/";
-
-}
